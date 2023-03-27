fdf07b631b32ea04872e75276d19eaf78646b54b
dyson <dyson0326@docomo.ne.jp>
2023-03-27 22:14:00 +0900

    例外処理時のメッセージを変更

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,12 +5,12 @@ public class Calculator {
 
         try {
             BigDecimal num1 = new BigDecimal("1");
-            BigDecimal num2 = new BigDecimal("3");
+            BigDecimal num2 = new BigDecimal("0");
             System.out.println("足し算の結果は"+num1.add(num2));
             System.out.println("引き算の結果は"+num1.subtract(num2));
             System.out.println("掛け算の結果は"+num1.multiply(num2));
             System.out.println("割り算の結果は"+num1.divide(num2));
         }catch (ArithmeticException e){
-            System.out.println("割り算の結果が無限小数になります");
+            System.out.println("割り算の結果が無限小数になるかnum2の値が0です");
         }}
 }
